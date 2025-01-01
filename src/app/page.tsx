5011698e0210ec7b52b2c00d0c76354ae13fc33c
Bhavesh S Singh <66252720+BhaveshSSingh@users.noreply.github.com>
2025-01-01 17:30:40 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,93 @@
-import Image from "next/image";
+"use client";
+
+import Vapi from "@vapi-ai/web";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Phone } from "lucide-react";
+import { useState } from "react";
+import { outBoundAction } from "@/actions/outbound";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+import { Label } from "@/components/ui/label";
+
+export const vapi = new Vapi("your-public-token");
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const [phoneNumber, setphoneNumber] = useState("");
+	const [name, setName] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const callHandler = async (name: string, phoneNumber:string) => {
+
+		await outBoundAction({ phone: phoneNumber, name: name });
+	};
+
+	return (
+		<main className="min-h-screen p-4 bg-background">
+			<div className="max-w-md mx-auto">
+				<Card>
+					<CardHeader>
+						<CardTitle>Contact Information</CardTitle>
+					</CardHeader>
+					<CardContent className="space-y-4">
+						<div className="space-y-2">
+							<label
+								htmlFor="name"
+								className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+							>
+								Name
+							</label>
+							<Input
+								id="name"
+								placeholder="Enter your name"
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+							/>
+						</div>
+						<div className="space-y-2">
+							<Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+								Phone
+							</Label>
+							<PhoneInput
+								country={"in"}
+								value={phoneNumber}
+								onChange={(phone) => setphoneNumber(phone)}
+								inputStyle={{
+									width: "100%",
+									height: "36px",
+									borderRadius: "6px ",
+									border: "1px solid hsl(var(--input)) ",
+									backgroundColor: "transparent",
+									fontSize: "14px",
+									paddingLeft: "48px",
+									outline: "none",
+									color: "hsl(var(--foreground))",
+								}}
+								containerStyle={{
+									width: "100%",
+									backgroundColor: "transparent",
+								}}
+								buttonStyle={{
+									backgroundColor: "transparent",
+									border: "1px solid hsl(var(--input))",
+									borderRight: "none",
+									borderRadius: "6px 0 0 6px",
+								}}
+								dropdownStyle={{
+									width: "300px",
+									backgroundColor: "hsl(var(--background))",
+									borderRadius: "0.5rem",
+									color: "hsl(var(--foreground))",
+								}}
+							/>
+						</div>
+						<Button onClick={() => callHandler(name, phoneNumber)}>
+							<Phone />
+							Call
+						</Button>
+					</CardContent>
+				</Card>
+			</div>
+		</main>
+	);
 }
